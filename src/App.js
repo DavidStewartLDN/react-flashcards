649685973f852623a1896f056cb67e748d9e092b
DavidStewartLDN <davidstewart.work@gmail.com>
2020-05-04 17:37:45 +0100

    first commit - Card Component added, no props passed

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Card from './Card/Card';
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      cards: [
+        {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
+        {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
+      ],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+    return card
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+  }
+}
+
+export default App;
