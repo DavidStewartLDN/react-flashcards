916b1f75acaf73966311e647503919ba2f0c4801
DavidStewartLDN <davidstewart.work@gmail.com>
2020-05-05 17:02:41 +0100

    firebase database can be fetched and cards are shown

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+import { DB_CONFIG } from './Config/Firebase/db_config';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(DB_CONFIG);
+}
 
 class App extends Component {
 
   constructor(props){
     super(props);
 
+    this.app = firebase
+    this.database = firebase.database().ref().child('cards');
+
     this.updateCard = this.updateCard.bind(this);
 
     this.state = {
-      cards: [
-        {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
-        {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
-      ],
+      cards: [],
       currentCard: {}
     }
   }
@@ -23,9 +30,17 @@ class App extends Component {
   componentWillMount(){
     const currentCards = this.state.cards;
 
-    this.setState({
-      cards: currentCards,
-      currentCard: this.getRandomCard(currentCards)
+    this.database.on('child_added', snap => {
+      currentCards.push({
+        id: snap.key,
+        eng: snap.val().eng,
+        han: snap.val().han,
+        pin: snap.val().pin
+      })
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards)
+      })
     })
   }
 
