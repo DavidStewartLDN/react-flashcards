20d39789d76c03e80b1fc4bdc66a6bd996bd5455
DavidStewartLDN <davidstewart.work@gmail.com>
2020-05-06 15:23:42 +0100

    successfully exported russian words to json using import_data_excel file

--- src/Data/import_data_excel.py
+++ src/Data/import_data_excel.py
@@ -1,8 +1,24 @@
 import openpyxl
+import json
 
 wb = openpyxl.load_workbook('russian_phrases.xlsx')
 ws = wb.active
+output_file_name = 'russian_cards.json'
+
+data = {}
+data['russian_cards'] = []
+counter = 1
 
 for row in ws.iter_rows(min_row=2, max_col=3):
-  for cell in row:
-    print(cell.value)
+  data['russian_cards'].append({
+    'id': counter,
+    'type': row[0].value,
+    'russian': row[1].value,
+    'english': row[2].value
+  })
+  counter +=1
+
+with open(output_file_name, 'w', encoding='utf-8') as outfile:
+    json.dump(data, outfile, indent=2, ensure_ascii=False)
+
+print(f"Data has successfully be output to {output_file_name}! Get learning :)")
