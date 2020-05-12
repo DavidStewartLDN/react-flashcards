e1f7f2a0ba619841cc9c941931432f2a838a4ab3
dariathompson <daria.kambalina@gmail.com>
2020-05-12 16:38:57 +0100

    checking if answer is right

--- src/Components/AnswerForm.js
+++ src/Components/AnswerForm.js
@@ -5,6 +5,7 @@ class AnswerForm extends Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
+    this.correct = 'green';
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,21 +16,34 @@ class AnswerForm extends Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
+    if(this.props.english === this.state.value){
+      alert('Correct answer');
+      this.correct = 'green';
+    }else{
+      alert('Wrong answer');
+      this.correct = 'red';
+      console.log(this.correct)
+    }
   }
+
+  // changeCss(){
+  //   if(this.correct === true){
+
+  //   }
+  // }
 
   render() {
     return (
-      <div>
+      <div className={
+        (this.correct === 'green') ? 'green' : 'red'
+      }>
         <div>
           <div className="thing">{this.props.native}</div>
         </div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
+        <form  
+        onSubmit={this.handleSubmit}>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
           <input type="submit" value="Submit" />
         </form>
       </div>
