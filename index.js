ad77d30cb99c315403af1c6d02647f0ec7b7782b
DavidStewartLDN <davidstewart.work@gmail.com>
2020-06-16 19:36:09 +0100

    added index.js for express server

---
+++ index.js
@@ -0,0 +1,146 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const russian_model = require('./russian_model')
+const italian_model = require('./italian_model')
+const mandarin_model = require('./mandarin_model')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+// Russian table
+
+app.get('/russian', (req, res) => {
+  russian_model.getRussian()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/russian/word/:id', (req, res) => {
+  russian_model.getRussianWord(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/russian/word', (req, res) => {
+  russian_model.createRussianWord(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/russian/word/:id', (req, res) => {
+  russian_model.deleteRussianWord(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+// Italian table
+
+app.get('/italian', (req, res) => {
+  italian_model.getItalian()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/italian/word/:id', (req, res) => {
+  italian_model.getItalianWord(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/italian/word', (req, res) => {
+  italian_model.createItalianWord(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/italian/word/:id', (req, res) => {
+  italian_model.deleteItalianWord(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+
+// Mandarin table
+
+app.get('/mandarin', (req, res) => {
+  mandarin_model.getMandarin()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/mandarin/word/:id', (req, res) => {
+  mandarin_model.getMandarinWord(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/mandarin/word', (req, res) => {
+  mandarin_model.createMandarinWord(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/mandarin/word/:id', (req, res) => {
+  mandarin_model.deleteMandarinWord(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
