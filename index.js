02b838136b6211cb993f3d64d1c1d669d2827d82
DavidStewartLDN <davidstewart.work@gmail.com>
2020-06-16 21:13:51 +0100

    added proxy to react and updated fetch requests

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const italian_model = require('./italian_model')
 const mandarin_model = require('./mandarin_model')
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')), express.json());
 
 // app.use(express.json())
 app.use(function (req, res, next) {
