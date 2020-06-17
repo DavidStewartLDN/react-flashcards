55dd4effb8b0b3116b83d6f6ea1ef58690df5e43
DavidStewartLDN <davidstewart.work@gmail.com>
2020-06-17 16:40:59 +0100

    trying with database_URI

--- russian_model.js
+++ russian_model.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
+const pg = require('pg');
+const pool = pg.connect(process.env.DATABASE_URL);
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT,
+// });
 
 const getRussian = () => {
   return new Promise(function(resolve, reject) {
